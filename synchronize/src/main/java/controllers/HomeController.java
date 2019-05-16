67c1a79b9996d50788a4acdca7275583decd6193
gihan <gihan@DESKTOP-KR3IV7O>
2019-05-16 13:42:24 +0530

    Added SweetAlerts

--- synchronize/src/main/java/controllers/HomeController.java
+++ synchronize/src/main/java/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,19 +23,47 @@ public class HomeController extends HttpServlet{
 	  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	  {
 
+		  
 	    HttpSession session = request.getSession(false);
 	    String hiddenToken = request.getParameter("myHiddenField");
 
 	    String csrfToken = SignInController.csrfTokenStore.get(session.getId());
-
+	 
+	    response.setContentType("text/html");
+	 
+	      
+	    PrintWriter out = response.getWriter();
+	    out.println("<html>");
+        out.println("<head>");
+	    out.println("<script src='./js/jquery.js'></script>");
+	    out.println("<script src='./js/sweetalert2.js'></script>");
+    	out.println("<link rel='stylesheet' type='text/css' href= './css/sweetalert2.css' />");
+    	out.println("</head>");
+    	out.println("<body>");
 	    if (csrfToken.equals(hiddenToken))
 	    {
-	      response.getWriter().append("Amount Transfered!");
+	    	out.println("<script>");
+	        out.println("Swal.fire(\r\n" + 
+	        		"  'Successful!',\r\n" + 
+	        		"  '***Amount Transferred***',\r\n" + 
+	        		"  'success'\r\n" + 
+	        		")");
+	       out.println("</script>");
+	        
+	        
 	    }
 	    else
 	    {
-	      response.getWriter().append("ERROR!");
-	    }
+	    	out.println("<script>");
+	        out.println("Swal.fire({\r\n" + 
+	        		"  type: 'error',\r\n" + 
+	        		"  title: 'ERROR...',\r\n" + 
+	        		"  text: 'Something went wrong!',\r\n" + 
+	        		"})");
+	        out.println("</script>");
+	     }
+	    out.println("</body>");
+	   
 	  }
 
 
